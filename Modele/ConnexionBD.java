1087d5c6fa910d74168fda187c13097a59698e0f
MathieuHerranzPerez <mathieu.herranzperez@gmail.com>
2017-12-13 14:38:59 +0100

    connexion BD a faire

---
+++ Modele/ConnexionBD.java
@@ -0,0 +1,12 @@
+package Modele;
+
+public class ConnexionBD {
+    private static ConnexionBD ourInstance = new ConnexionBD();
+
+    public static ConnexionBD getInstance() {
+        return ourInstance;
+    }
+
+    private ConnexionBD() {
+    }
+}
